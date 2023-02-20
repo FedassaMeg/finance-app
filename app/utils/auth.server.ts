cbfc15b5bb4333958e126c0e427afabc2564dac2
Thomas Meg <fmegerssa@protonmail.com>
2023-02-20 12:23:41 -0800

    feat: Add session management

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,8 +1,35 @@
-import { json } from "@remix-run/node";
+import { redirect, json, createCookieSessionStorage } from "@remix-run/node";
 import bcrypt from "bcryptjs";
 import prisma from "../lib/db.server";
 import type { LoginForm, RegisterForm } from "./types.server";
 import { createUserAccount } from "./user.server";
+
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET must be set!");
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "finance-app-session",
+    secure: process.env.NODE_ENV === "production",
+    secrets: [sessionSecret],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+});
+
+export async function createUserSession(userId: string, redirectTo: string) {
+  const session = await storage.getSession();
+  session.set("userId", userId);
+  return redirect(redirectTo, {
+    headers: {
+      "Set-Cookie": await storage.commitSession(session),
+    },
+  });
+}
 
 export async function register(user: RegisterForm) {
   const exists = await prisma.user.count({
@@ -27,6 +54,8 @@ export async function register(user: RegisterForm) {
       }
     );
   }
+
+  return createUserSession((await account).id, "/");
 }
 
 export async function login({ email, password }: LoginForm) {
@@ -37,5 +66,5 @@ export async function login({ email, password }: LoginForm) {
   if (!account || !(await bcrypt.compare(password, user.password)))
     return json({ error: "Incorrect login" }, { status: 400 });
 
-  return { id: account.id, email };
+  return createUserSession(account.id, "/");
 }
