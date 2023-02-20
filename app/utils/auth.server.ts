6d150d236ddf330c5af9e5f302153260ba423d04
Thomas Meg <fmegerssa@protonmail.com>
2023-02-20 12:15:05 -0800

    feat: Add login call

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,6 +1,7 @@
 import { json } from "@remix-run/node";
+import bcrypt from "bcryptjs";
 import prisma from "../lib/db.server";
-import type { RegisterForm } from "./types.server";
+import type { LoginForm, RegisterForm } from "./types.server";
 import { createUserAccount } from "./user.server";
 
 export async function register(user: RegisterForm) {
@@ -26,4 +27,15 @@ export async function register(user: RegisterForm) {
       }
     );
   }
+}
+
+export async function login({ email, password }: LoginForm) {
+  const account = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (!account || !(await bcrypt.compare(password, user.password)))
+    return json({ error: "Incorrect login" }, { status: 400 });
+
+  return { id: account.id, email };
 }
