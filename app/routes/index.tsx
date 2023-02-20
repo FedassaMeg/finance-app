0ac8712f72a23740ffdf7e90e9fc245f1223e399
Thomas Meg <fmegerssa@protonmail.com>
2023-02-20 10:12:18 -0800

    Add and configure tailwind
    Concurrently added to allow tailwind build to run alongside remix build process

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+    <div className="h-screen bg-slate-700 flex justify-center items-center">
+      <h1 className="text-blue-600 font-extrabold text-5xl">Welcome to Remix</h1>
       <ul>
         <li>
           <a
