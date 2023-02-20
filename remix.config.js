e9518f83357a93f4cee691bf951c41160be9b6b2
Thomas Meg <fmegerssa@protonmail.com>
2023-02-20 11:01:52 -0800

    chore: format file

--- remix.config.js
+++ remix.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   future: {
-    unstable_tailwind:true
-  }
+    unstable_tailwind: true,
+  },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
