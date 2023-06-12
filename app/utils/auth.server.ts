97f8584d4313a9a3c0ccc36301f5450e4fc88fd2
Thomas Meg <fmegerssa@protonmail.com>
2023-06-12 13:56:00 -0700

    feat: Add functions to retrieve user and logout

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -63,8 +63,59 @@ export async function login({ email, password }: LoginForm) {
     where: { email },
   });
 
-  if (!account || !(await bcrypt.compare(password, user.password)))
+  if (!account || !(await bcrypt.compare(password, account.password)))
     return json({ error: "Incorrect login" }, { status: 400 });
 
   return createUserSession(account.id, "/");
+}
+
+export async function requiredUserId(
+  request: Request,
+  redirectTo: string = new URL(request.url).pathname
+) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+  if (!userId || typeof userId !== "string") {
+    const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
+    throw redirect(`/login?${searchParams}`);
+  }
+
+  return userId;
+}
+
+function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get("Cookie"));
+}
+
+async function getUserId(request: Request) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+  if (!userId || typeof userId !== "string") return null;
+  return userId;
+}
+
+export async function getUser(request: Request) {
+  const userId = await getUserId(request);
+  if (typeof userId !== "string") {
+    return null;
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: parseInt(userId, 10) },
+      select: { id: true, email: true },
+    });
+    return user;
+  } catch {
+    throw logout(request);
+  }
+}
+
+export async function logout(request: Request) {
+  const session = await getUserSession(request);
+  return redirect("/login", {
+    headers: {
+      "Set-Cookie": await storage.destroySession(session),
+    },
+  });
 }
