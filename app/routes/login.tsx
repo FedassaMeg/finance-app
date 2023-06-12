e4d6f8a075b79159a8fedb335433af48094e5087
Thomas Meg <fmegerssa@protonmail.com>
2023-06-12 13:53:45 -0700

    feat: Add error and data state to login form

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,5 +1,11 @@
-import React, { useState } from "react";
-import { ActionFunction, json } from "@remix-run/node";
+import React, { useState, useRef, useEffect, useReducer } from "react";
+import { useActionData } from "@remix-run/react";
+import {
+  ActionFunction,
+  json,
+  LoaderFunction,
+  redirect,
+} from "@remix-run/node";
 import { FormField } from "~/components/form-field";
 import { Layout } from "~/components/layout";
 import {
@@ -7,7 +13,11 @@ import {
   validateName,
   validatePassword,
 } from "~/utils/validators.server";
-import { login, register } from "~/utils/auth.server";
+import { getUser, login, register } from "~/utils/auth.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return (await getUser(request)) ? redirect("/") : null;
+};
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData();
@@ -63,13 +73,41 @@ export const action: ActionFunction = async ({ request }) => {
 };
 
 export default function Login() {
+  const actionData = useActionData();
+  const firstLoad = useRef(true);
+  const [errors, setErrors] = useState(actionData?.errors || {});
+  const [formError, setFormError] = useState(actionData?.error || "");
   const [action, setAction] = useState("login");
   const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    firstName: "",
-    lastName: "",
+    email: actionData?.fields?.email || "",
+    password: actionData?.fields?.password || "",
+    firstName: actionData?.fields?.firstName || "",
+    lastName: actionData?.fields?.lastName || "",
   });
+
+  useEffect(() => {
+    if (!firstLoad.current) {
+      const newState = {
+        email: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+      };
+      setErrors(newState);
+      setFormError("");
+      setFormData(newState);
+    }
+  }, [action]);
+
+  useEffect(() => {
+    if (!firstLoad.current) {
+      setFormError("");
+    }
+  }, [formData]);
+
+  useEffect(() => {
+    firstLoad.current = false;
+  }, []);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     setFormData((form) => ({
@@ -91,6 +129,7 @@ export default function Login() {
         </p>
 
         <form method="POST" className="rounded-2xl bg-gray-200 p-6 w-96">
+          <div className="text-xs font-semibold text-center tracking-wide text-red-500 w-full">{formError}</div>
           {action === "register" && (
             <>
               <FormField
@@ -98,12 +137,14 @@ export default function Login() {
                 label="First Name"
                 onChange={handleInputChange}
                 value={formData.firstName}
+                error={errors?.firstName}
               />
               <FormField
                 htmlFor="lastName"
                 label="Last Name"
                 onChange={handleInputChange}
                 value={formData.lastName}
+                error={errors?.lastName}
               />
             </>
           )}
@@ -112,12 +153,14 @@ export default function Login() {
             label="Email"
             value={formData.email}
             onChange={handleInputChange}
+            error={errors?.email}
           />
           <FormField
             htmlFor="password"
             label="Password"
             value={formData.password}
             onChange={handleInputChange}
+            error={errors?.password}
           />
           <div className="w-full text-center">
             <button
