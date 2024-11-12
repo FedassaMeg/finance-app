0e6843e30208adca95f8f5f82e823046ee76f2cd
Fedassa Megerssa <fedassa.tadewos00@gmail.com>
2024-11-12 10:20:23 -0800

    feat: remove remix boilerplate code

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,5 @@
 import { LoaderFunction } from "@remix-run/node";
 import { requiredUserId } from "../utils/auth.server";
-import { Layout } from "../root";
 
 // export const loader: LoaderFunction = async ({ request }) => {
 //   await requiredUserId(request);
@@ -9,37 +8,10 @@ import { Layout } from "../root";
 
 export default function Index() {
   return (
-    <Layout>
-      <div className="h-screen bg-slate-700 flex justify-center items-center">
-        <h1 className="text-blue-600 font-extrabold text-5xl">
-          Welcome to Remix
-        </h1>
-        <ul>
-          <li>
-            <a
-              target="_blank"
-              href="https://remix.run/tutorials/blog"
-              rel="noreferrer"
-            >
-              15m Quickstart Blog Tutorial
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              href="https://remix.run/tutorials/jokes"
-              rel="noreferrer"
-            >
-              Deep Dive Jokes App Tutorial
-            </a>
-          </li>
-          <li>
-            <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-              Remix Docs
-            </a>
-          </li>
-        </ul>
-      </div>
-    </Layout>
+    <div className="h-screen bg-slate-700 flex justify-center items-center">
+      <h1 className="text-blue-600 font-extrabold text-5xl">
+        Dashboard 
+      </h1>
+    </div>
   );
 }
